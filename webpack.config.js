6511215caa35855fb7f584a1d901b0633e5f45da
M-Shehu <muhammadshehu@Shehus-MBP.attlocal.net>
2019-06-05 23:44:51 -0500

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: ["@babel/polyfill", `${SRC_DIR}/index.jsx`],
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {extensions: ['.js', '.jsx']},
+  devServer: {
+    contentBase: DIST_DIR,
+    compress: true,
+    port: 9000
+  }
+};
